9e385e0d840643a32e9ba53c27d4c6bd416b500c
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2021-03-30 04:00:06 +0530

    code cleanup

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ WORKDIR /go/src/github.com/abhijitWakchaure/mock-api-server-go/
 COPY . .
 RUN go get -u github.com/gobuffalo/packr/v2/packr2
 RUN packr2
-RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o dist/mock-api-server-go-linux-amd64
+RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o dist/app
 
 
 FROM alpine
 WORKDIR /app
-COPY --from=builder /go/src/github.com/abhijitWakchaure/mock-api-server-go/dist/mock-api-server-go-linux-amd64 .
+COPY --from=builder /go/src/github.com/abhijitWakchaure/mock-api-server-go/dist/app .
 EXPOSE 8080
-CMD ["./mock-api-server-go-linux-amd64"]
+CMD ["./app"]
--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 packr2
 appName="mock-api-server-go"
+appVersion="v1.0.0"
 export CGO_ENABLED=0
 echo "### Building for platform: linux/amd64"
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-linux-amd64
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-${appVersion}-linux-amd64
 echo "### Building for platform: windows/amd64"
-env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-win-amd64.exe
+env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-${appVersion}-win-amd64.exe
 echo "### Building for platform: darwin/amd64"
-env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-darwin-amd64
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o dist/${appName}-${appVersion}-darwin-amd64
 packr2 clean
 echo "### Building docker image"
 docker build -t abhijitwakchaure/mock-api-server-go .
