747a6904514d952b3c4216f1b17bd13caaaabba8
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2021-03-30 19:23:29 +0530

    fixed: CORS issue from browser

--- user/controller.go
+++ user/controller.go
@@ -112,6 +112,7 @@ func (c *Controller) ListUsers(w http.ResponseWriter, r *http.Request) {
 // EncodeResponse ...
 func EncodeResponse(w http.ResponseWriter, code int, res interface{}) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(res)
 }
